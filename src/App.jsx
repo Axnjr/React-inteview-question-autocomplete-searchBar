179c27d63034baf9a9e6afa39c17159215a372b5
Yakshit Chhipa <111436589+Axnjr@users.noreply.github.com>
2023-03-07 21:19:57 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,67 @@
-import React, { useState, useEffect } from 'react'
+import { useState , useEffect, Suspense } from 'react'
 import axios from 'axios'
+import useApi from './useApi'
 
 export default function App() {
 
-	const [selItem, setSelItem] = useState(null)
-	const [data, setData] = useState([])
+	const url = "https://random-data-api.com/api/users/random_user?size=50"
+	const [query1,setQuery1] = useState("")
+	const [query2,setQuery2] = useState("")
+	const [fetchedData,setFetchedData] = useState([])
+	const [fetchedData2,setFetchedData2] = useApi(url)
 
-	async function ApiSimulator() {
+	function FetchedResults(){
+		return(
+			<div>
+			{
+				!fetchedData.length<1 ? 
+				<div>
+				{
+					fetchedData.map((ele,id) => <li key={id}>{ele.first_name}</li>)
+				}
+				</div>
+				:
+				<h1>No results found</h1>
+			}
+			</div>
+		)
+	}
+
+	async function FetchDataFromApi(){
 		return await axios.get("https://random-data-api.com/api/users/random_user?size=50")
 	}
 
 	useEffect(() => {
-		if (selItem) {
-			ApiSimulator()
-				.then(res => {
-					setData(res.data.filter(ele => ele.first_name.includes(selItem)))
-				})
-				.catch(err => {
-					alert("Unexpected error occured !");
-					console.log(err);
-				});
+		if(fetchedData2){
+			setFetchedData(fetchedData2.filter(items => items.first_name.includes(query1)))
 		}
-	}, [selItem])
+	} , [query1])
 
-	return (
-		<div>
-			<input placeholder='Type names ...' type="text" onChange={(e) => { setSelItem(e.target.value) }}></input>
-			<div>
-				{
-				!selItem ?
-					<div>Search Results appear here</div> :
-					<div>
-						{
-							!data.length<=0 ?
-								<div>
-									{
-										data.map((ele, id) => {
-											return (
-												<li key={id.toString()}>{ele.first_name}</li>
-											)
-										})
-									}
-								</div>
-								:
-								<div>No results found</div>
-						}
-					</div>
-				}
-			</div>
-		</div>
-	)
+	useEffect(() => {
+		FetchDataFromApi()
+		.then(res => {
+			setFetchedData(res.data.filter(items => items.first_name.includes(query2)))
+		})
+		.catch(err => {
+			console.log(err)
+		})
+	} , [query2])
+
+	function Loading(){
+		return <h1>Loading...</h1>
+	}
+
+  return (
+	<div>
+		<input placeholder='Already present results..' type="text" onChange={(e) => { setQuery1(e.target.value) }} ></input>
+		<input placeholder='Fresh results..' type="text" onChange={(e) => { setQuery2(e.target.value) }} ></input>
+
+		{
+			query1 || query2 ? 
+			<FetchedResults/>
+			:
+			<h2>All Search results appear here. Search from one input at a time</h2>
+		}
+	</div>
+  )
 }
